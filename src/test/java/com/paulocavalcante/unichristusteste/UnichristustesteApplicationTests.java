d508eeefdbb0be20d2bad6b57a57256a5005c12d
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2023-05-08 19:49:21 -0300

    Início projeto teste técnico.

---
+++ src/test/java/com/paulocavalcante/unichristusteste/UnichristustesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulocavalcante.unichristusteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnichristustesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
